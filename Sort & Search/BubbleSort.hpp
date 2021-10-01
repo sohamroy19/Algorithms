c876b3cc40c9a46726ac584ea5cc9f947bcb52b3
royS <72430659+sohamroy19@users.noreply.github.com>
2021-10-01 10:43:50 +0530

    Added Bubble Sort

---
+++ Sort & Search/BubbleSort.hpp
@@ -0,0 +1,25 @@
+/**
+ * Worst case : O(n^2) comparisons, O(n^2) swaps
+ * Average    : O(n^2) comparisons, O(n^2) swaps
+ * Best case  :  O(n)  comparisons,  O(1)  swaps
+ * 
+ * Space      : O(1)
+ */
+
+#include <vector>
+
+template <class T>
+void bubbleSort(std::vector<T> &arr) {
+    for (int i = arr.size(); i > 1; --i) {
+        bool swapped = false;
+
+        for (int j = 1; j < i; ++j) {
+            if (arr[j - 1] > arr[j]) {
+                std::swap<T>(arr[j - 1], arr[j]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) return; // if already done sorting
+    }
+}
--- Sort & Search/InsertionSort.hpp
+++ Sort & Search/InsertionSort.hpp
@@ -6,19 +6,20 @@
  * Space      : O(1)
  */
 
+#include <iostream>
 #include <vector>
 
 template <class T>
 void insertionSort(std::vector<T> &arr) {
     for (int i = 1; i < arr.size(); ++i) {
-        T temp = arr[i];
+        T temp = std::move(arr[i]);
 
         int j = i - 1;
-        while(j > 0 && arr[j] > arr[i]) {
-            arr[j + 1] = arr[j]; // shift larger element rightwards
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = std::move(arr[j]); // shift larger element rightwards
             --j;
         }
 
-        arr[j + 1] = temp;
+        arr[j + 1] = std::move(temp);
     }
 }
