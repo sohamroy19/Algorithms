656e5391d67b475bfdfcd9535e31f0e1ab9ac81d
royS <72430659+sohamroy19@users.noreply.github.com>
2021-10-15 15:28:49 +0530

    Improved structure

--- Data Structures/Matrix.hpp
+++ Data Structures/Matrix.hpp
@@ -4,18 +4,20 @@
 
 template <class T>
 class Mat {
+private:
+    std::vector<std::vector<T>> a;
+
 public:
     int const n, m;
-    std::vector<std::vector<T>> a;
 
     Mat(int nInit, int mInit)
         : n(nInit), m(mInit),
-          a(std::vector<std::vector<T>>(n, std::vector<T>(m))) {}
+          a(std::vector<std::vector<T>>(nInit, std::vector<T>(mInit))) {}
 
     Mat(std::vector<std::vector<T>> v)
-        : n(v.size()), m(n > 0 ? v[0].size() : 0), a(v) {}
+        : n(v.size()), m(v.empty() ? 0 : v[0].size()), a(v) {}
 
-    Mat& operator=(const Mat &b) {
+    Mat &operator=(const Mat &b) {
         assert(n == b.n && m == b.m);
 
         if (this != &b) {
@@ -26,13 +28,13 @@ public:
     }
 
     T &operator()(int i, int j) {
-        assert(0 <= i && i < n);
+        assert(0 <= i && i < n && 0 <= j && j < m);
 
         return a[i][j];
     }
 
     T operator()(int i, int j) const {
-        assert(0 <= i && i < n);
+        assert(0 <= i && i < n && 0 <= j && j < m);
 
         return a[i][j];
     }
@@ -50,7 +52,7 @@ public:
     Mat operator+(const Mat &b) {
         assert(n == b.n && m == b.m);
 
-        Mat ans = Mat(n, m);
+        Mat ans(n, m);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 ans(i, j) = (a[i][j] + b(i, j));
@@ -63,7 +65,7 @@ public:
     Mat operator-(const Mat &b) {
         assert(n == b.n && m == b.m);
 
-        Mat ans = Mat(n, m);
+        Mat ans(n, m);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 ans(i, j) = (a[i][j] - b(i, j));
@@ -76,7 +78,7 @@ public:
     Mat operator*(const Mat &b) {
         assert(m == b.n);
 
-        Mat ans = Mat(n, b.m);
+        Mat ans(n, b.m);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 for (int k = 0; k < b.m; k++) {
@@ -152,20 +154,4 @@ std::ostream &operator<<(std::ostream &os, Mat<T> const &mat) {
     os << mat(mat.n - 1, mat.m - 1) << ")";
 
     return os;
-}
-
-int main() {
-    Mat<int> eg({{1, 2}, {3, 4}});
-    Mat<int> id(2, 2);
-    id.makeIdentity();
-
-    std::cout << "eg      : " << eg << "\n";
-    std::cout << "id      : " << id << "\n\n";
-
-    std::cout << "eg + id : " << eg + id << "\n";
-    std::cout << "eg - id : " << eg - id << "\n";
-    std::cout << "eg * id : " << eg * id << "\n\n";
-
-    std::cout << "id ^ 2  : " << id.pow(2) << "\n";
-    std::cout << "eg ^ 2  : " << eg.pow(2) << "\n\n";
 }
