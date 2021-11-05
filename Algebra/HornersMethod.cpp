a015b3f1cbaaf692d2a9c5c2af168a14925516a7
royS <72430659+sohamroy19@users.noreply.github.com>
2021-11-05 14:14:08 +0530

    Added Horner's Method

---
+++ Algebra/HornersMethod.cpp
@@ -0,0 +1,39 @@
+/**
+ * Time  : O(n)
+ * Space : O(1)
+ */
+
+#include <iostream>
+#include <vector>
+
+template <class T>
+T horner(std::vector<T> &coeffs, T x) {
+    double result = 0;
+
+    for (int i = coeffs.size() - 1; i >= 0; --i) {
+        result = result * x + coeffs[i];
+    }
+    
+    return result;
+}
+
+int main() {
+    std::cout << "Enter the degree of the polynomial: ";
+    int n;
+    std::cin >> n;
+
+    std::cout << "\nEnter the coefficients of the polynomial:\n";
+    std::vector<double> coeffs(n + 1);
+    for (int i = 0; i <= n; ++i) {
+        std::cout << "a_" << i << ": ";
+        std::cin >> coeffs[i];
+    }
+
+    std::cout << "\nEnter the value of x: ";
+    double x;
+    std::cin >> x;
+
+    std::cout << "\nValue of the polynomial at x = " << x << ": " << horner(coeffs, x) << "\n";
+
+    return 0;
+}
