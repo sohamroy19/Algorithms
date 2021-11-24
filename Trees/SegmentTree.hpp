c2bc674020b844ec6240fb70a1fbaed24bf7e587
royS <72430659+sohamroy19@users.noreply.github.com>
2021-11-24 23:54:18 +0530

    Added Segment Tree

---
+++ Trees/SegmentTree.hpp
@@ -0,0 +1,72 @@
+#ifndef SEGMENT_TREE_HPP
+#define SEGMENT_TREE_HPP
+
+#include <vector>
+
+template <class T>
+class SegmentTree {
+private:
+    std::vector<T> tree;
+    int size;
+    T (*merge) (T, T);
+
+public:
+    SegmentTree(const std::vector<T> &arr, T (*f)(T, T))
+        : tree(std::vector<T>(4 * arr.size())), size(arr.size()), merge(f) {
+        build(arr, 0, 0, size - 1);
+    }
+
+    void build(const std::vector<T> &arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+            cout << node << " " << tree[node] << endl;
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        build(arr, 2 * node + 1, start, mid);
+        build(arr, 2 * node + 2, mid + 1, end);
+        tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
+
+        cout << node << " " << tree[node] << endl;
+    }
+
+    T query(int start, int end) {
+        return query(0, 0, size - 1, start, end);
+    }
+
+    T query(int node, int start, int end, int left, int right) {
+        if (left > end || right < start) {
+            return 0;
+        }
+
+        if (start >= left && end <= right) {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        return merge(query(2 * node + 1, start, mid, left, right), query(2 * node + 2, mid + 1, end, left, right));
+    }
+
+    void update(int pos, T val) {
+        update(0, 0, size - 1, pos, val);
+    }
+
+    void update(int node, int start, int end, int pos, T val) {
+        if (start == end) {
+            tree[node] = val;
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        if (pos <= mid) {
+            update(2 * node + 1, start, mid, pos, val);
+        } else {
+            update(2 * node + 2, mid + 1, end, pos, val);
+        }
+
+        tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
+    }
+};
+
+#endif /* SEGMENT_TREE_HPP */
